2a2bccd19fab3f528c353a2f497fc0cdd54c701d
Mouad Sodor <sodormouad@gmail.com>
2023-04-24 17:41:04 +0100

    edit

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int n = printf("hel");
-	printf("\n%d\n", n);
-}
---
+++ print_base.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_base - print a decimal number to anny base you decide
+ * @nbr: the number to print
+ * @base: base
+ * @spes: the specifier to choose either lower base or upper
+ * @count: pointer to the count of printed bytes
+ */
+
+void print_base(unsigned int nbr, unsigned int base, char spes, int *count)
+{
+	char *lower = "0123456789abcdef";
+	char *upper = "0123456789ABCDEF";
+	char *list;
+
+	list = spes == 'X' ? upper : lower;
+
+
+	if (nbr < 0)
+	{
+		print_c('-', count);
+		nbr = -nbr;
+	}
+
+	if (nbr < base)
+		print_c(list[nbr], count);
+
+	else
+	{
+		print_base((nbr / base), base, spes, count);
+		print_base((nbr % base), base, spes, count);
+	}
+}
