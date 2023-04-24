ffccc587fc89569536d760979ad538d15a7226ce
Mouad Sodor <sodormouad@gmail.com>
2023-04-24 16:43:00 +0100

    edit

--- main.h
+++ main.h
@@ -6,14 +6,14 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-int _printf(const char *format, ...);
 /* Declaration */
-int _strchr(char *str, char c);
-void print_base(unsigned int nbr, unsigned int base, char spes, int *count);
-void print_d(int nbr, int *count);
-void print_s(char *str, int *count);
+int _printf(const char *format, ...);
 void print_c(int c, int *count);
-void print_r(char *str, int *count);
+void print_s(char *str, int *count);
+// int _strchr(char *str, char c);
+// void print_base(unsigned int nbr, unsigned int base, char spes, int *count);
+// void print_d(int nbr, int *count);
+// void print_r(char *str, int *count);
 
 
 #endif
