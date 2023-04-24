43f5a7563c21b2443e2c66f3ba0a103a2220753e
Mouad Sodor <sodormouad@gmail.com>
2023-04-24 18:09:02 +0100

    edit

---
+++ print/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Includes */
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* Declaration */
+int _printf(const char *format, ...);
+void print_c(int c, int *count);
+void print_s(char *str, int *count);
+int _strchr(char *str, char c);
+void print_base(unsigned int nbr, unsigned int base, char spes, int *count);
+void print_d(int nbr, int *count);
+void print_r(char *str, int *count);
+
+
+#endif
--- printf.c
+++ printf.c
@@ -1,48 +1,75 @@
 #include "main.h"
 
-/**
- * format_handler - handle the format
- * @args: args
- * @count: poiner to a count
- * @pres: precision
- */
-
-void format_handler(va_list args, char pres, int *count)
+void print_address(va_list ap, int *counter)
 {
-	if (pres == 'c')
-		print_c(va_arg(args, int), count);
-	else if (pres == 's')
-		print_s(va_arg(args, char*), count);
-	else if (pres == '%')
-		print_c('%', count);
-	else if (pres == 'd' || pres == 'i')
-		print_d(va_arg(args, int), count);
-	else if (pres == 'r')
-		print_r(va_arg(args, char*), count);
-	else if (pres == 'x' || pres == 'X')
-		print_base(va_arg(args, int), 16, pres, count);
-	else if (pres == 'p')
+	unsigned long int addrr;
+
+	addrr = va_arg(ap, unsigned long int);
+	if (addrr)
 	{
-		print_s("0x", count);
-		print_base(va_arg(args, int), 16, 'x', count);
+		*counter += putstr("0x", 0);
+		put_address(addrr, counter);
 	}
 	else
-		print_c(pres ,count);
+		*counter += putstr("(nil)", 0);
 }
 
 /**
- * _printf - function printf
- * @format: format
- * Return: the number of bytes printed
+ *select_id - executes the right function
+ *@ap: the aprgument pointer of the variadic function
+ *@c: the character to check
+ *@counter: character counter
+ *Return: returns nothing
+ */
+
+void select_id(va_list ap, int c, int *counter)
+{
+	if (c == 'c')
+		*counter += _putchar(va_arg(ap, int));
+	else if (c == 's')
+		*counter += putstr(va_arg(ap, char *), 0);
+	else if (c == '%')
+		*counter += _putchar('%');
+	else if (c == 'd' || c == 'i')
+		putnbr(va_arg(ap, int), counter);
+	else if (c == 'b')
+		putnbr_binary(va_arg(ap, int), counter);
+	else if (c == 'u')
+		putunbr_octal(va_arg(ap, unsigned int), 10, counter);
+	else if (c == 'o')
+		putunbr_octal(va_arg(ap, unsigned int), 8, counter);
+	else if (c == 'x')
+		putnbr_hex(va_arg(ap, unsigned int), 0, counter);
+	else if (c == 'X')
+		putnbr_hex(va_arg(ap, unsigned int), 1, counter);
+	else if (c == 'S')
+		*counter += putstr(va_arg(ap, char *), 1);
+	else if (c == 'p')
+		print_address(ap, counter);
+	else if (c == 'r')
+		*counter += rev_str(va_arg(ap, char *));
+	else if (c == 'R')
+		*counter += rot13(va_arg(ap, char *));
+	else
+	{
+		*counter += _putchar('%');
+		*counter += _putchar(c);
+	}
+}
+
+/**
+ *_printf - prints a formatted string
+ *@format: the format of the string to print
+ *Return: the number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
 	int i = 0;
-	int count = 0;
-	va_list args;
+	int counter = 0;
+	va_list ap;
 
-	va_start(args, format);
+	va_start(ap, format);
 	if (!format)
 		return (-1);
 	while (format[i])
@@ -52,12 +79,12 @@ int _printf(const char *format, ...)
 			i++;
 			if (!format[i])
 				return (-1);
-			format_handler(args, format[i], &count);
+			select_id(ap, format[i], &counter);
 		}
 		else
-			print_c(format[i], &count);
+			counter += _putchar(format[i]);
 		i++;
 	}
-	va_end(args);
-	return (count);
+	va_end(ap);
+	return (counter);
 }
