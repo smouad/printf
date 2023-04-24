bc82c1e84df027ed491d387c15d0a4b814b4b0dd
YusufLisawi <yusufisawi@gmail.com>
2023-04-24 19:09:28 +0100

    Updates

--- print/main.h
+++ print/main.h
@@ -11,9 +11,9 @@ int _printf(const char *format, ...);
 void _putchar(int c, int *count);
 void _putstr(char *str, int *count);
 int _strchr(char *str, char c);
+void _putrev(char *str, int *count);
+void _putrot13(char *str, int *count);
 void _putnbr_base(unsigned int nbr, unsigned int base, char spes, int *count);
 void print_d(int nbr, int *count);
-void _putrev(char *str, int *count);
-
 
 #endif
--- print/printf_utils.c
+++ print/printf_utils.c
@@ -25,11 +25,7 @@ void _putstr(char *str, int *count)
 {
 	int i;
 
-	if (!str)
-	{
-		str = "(null)";
-		return ;
-	}
+	str = !str ? "(null)" : str;
 	for (i = 0; str[i]; i++)
 	{
 		if ((str[i] > 0 && str[i] < 32) || str[i] >= 127)
@@ -64,4 +60,28 @@ void _putrev(char *str, int *count)
 		len++;
 	while (len--)
 		_putchar(str[len], count);
+}
+
+/**
+ * _putrot13 - prints a string on rot13
+ * @str: string
+ * @count: count
+ */
+
+void _putrot13(char *str, int *count)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm')
+		|| (str[i] >= 'A' && str[i] <= 'M'))
+			_putchar(str[i] + 13, count);
+
+		else if ((str[i] >= 'n' && str[i] <= 'z')
+		|| (str[i] >= 'N' && str[i] <= 'Z'))
+			_putchar(str[i] - 13, count);
+		else
+			_putchar(str[i], count);
+	}
 }
