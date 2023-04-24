64220a49f6186abc83de238d0c90fdefdd5bc2a7
YusufLisawi <yusufisawi@gmail.com>
2023-04-24 19:18:31 +0100

    Updates

--- print/main.h
+++ print/main.h
@@ -10,7 +10,6 @@
 int _printf(const char *format, ...);
 void _putchar(int c, int *count);
 void _putstr(char *str, int *count);
-int _strchr(char *str, char c);
 void _putrev(char *str, int *count);
 void _putrot13(char *str, int *count);
 void _putnbr_base(unsigned int nbr, unsigned int base, char spes, int *count);
