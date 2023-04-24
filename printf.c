3d41abb04b329b62bde6a3308ae23eb258161eac
Mouad Sodor <sodormouad@gmail.com>
2023-04-24 18:22:19 +0100

    edit

--- printf.c
+++ printf.c
@@ -1,5 +1,19 @@
 #include "main.h"
 
+void print_address(va_list ap, int *counter)
+{
+	unsigned long int addrr;
+
+	addrr = va_arg(ap, unsigned long int);
+	if (addrr)
+	{
+		*counter += putstr("0x", 0);
+		put_address(addrr, counter);
+	}
+	else
+		*counter += putstr("(nil)", 0);
+}
+
 /**
  *select_id - executes the right function
  *@ap: the aprgument pointer of the variadic function
@@ -16,6 +30,26 @@ void select_id(va_list ap, int c, int *counter)
 		*counter += putstr(va_arg(ap, char *), 0);
 	else if (c == '%')
 		*counter += _putchar('%');
+	else if (c == 'd' || c == 'i')
+		putnbr(va_arg(ap, int), counter);
+	else if (c == 'b')
+		putnbr_binary(va_arg(ap, int), counter);
+	else if (c == 'u')
+		putunbr_octal(va_arg(ap, unsigned int), 10, counter);
+	else if (c == 'o')
+		putunbr_octal(va_arg(ap, unsigned int), 8, counter);
+	else if (c == 'x')
+		putnbr_hex(va_arg(ap, unsigned int), 0, counter);
+	else if (c == 'X')
+		putnbr_hex(va_arg(ap, unsigned int), 1, counter);
+	else if (c == 'S')
+		*counter += putstr(va_arg(ap, char *), 1);
+	else if (c == 'p')
+		print_address(ap, counter);
+	else if (c == 'r')
+		*counter += rev_str(va_arg(ap, char *));
+	else if (c == 'R')
+		*counter += rot13(va_arg(ap, char *));
 	else
 	{
 		*counter += _putchar('%');
